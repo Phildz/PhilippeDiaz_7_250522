33bf020207dc353d8648770e7183b5903759e0b8
Phildz <phd64@hotmail.fr>
2022-05-26 20:41:56 +0200

    Configuration du projet avant étape CRUD (#1)

---
+++ backend/app.js
@@ -0,0 +1,53 @@
+// IMPORTS
+
+const express = require('express');
+const bodyParser = require('body-parser');
+//const mongoose = require('mongoose');
+const path = require('path');
+//const sauceRoutes = require('./routes/sauces');
+//const userRoutes = require('./routes/user');
+
+require('dotenv').config();
+
+// CONSTANTES
+
+const app = express();
+
+
+/*mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));*/
+
+
+// APP
+
+app.use(express.json());
+
+// Gestion de la sécurité
+app.use((req, res, next) => {
+    // permet l'accès à l'API depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // permet d'ajouter des headers spécifiques aux requêtes envoyées vers l'API
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');        
+    // permet d'envoyer des requêtes avec les méthodes mentionnée
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+
+// création middleware répondant aux req envoyées à /images
+// et servant le dossier statique /images en utilisant express.static()
+// chemin déterminé par la const path, utilisation méthode .join() avec
+// dirnamme = nom dossier où on se trouve dans lequel on rajout images
+//app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// on indique que l'on va utiliser les routeurs définis par sauceRoutes et userRoutes
+//app.use('/api/sauces', sauceRoutes);
+
+//app.use('/api/auth', userRoutes);
+
+
+module.exports = app;
---
+++ backend/config/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
